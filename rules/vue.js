b9ef67e07dc6edb74110f5ef69edf1ac4962c37d
luooooob <contact@jiang-yan.com>
2021-11-24 17:40:01 +0800

    Add vue support

---
+++ rules/vue.js
@@ -0,0 +1,14 @@
+module.exports = {
+  overrides: [
+    {
+      files: ["*.vue"],
+      parser: "vue-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser"
+      },
+      extends: [
+        "plugin:vue/vue3-recommended"
+      ]
+    }
+  ]
+}
